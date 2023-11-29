ce8dcd1b5628b750d9d7011f7fbbbae41633f833
meltphace7 <brd9326@protonmail.com>
2023-11-29 09:54:18 -0800

    fixed form error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        <div id="overlays"></div>
+        <div id="overlay"></div>
         <Main />
         <NextScript />
       </body>
