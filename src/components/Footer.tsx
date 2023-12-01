34ec5d5011eb00f43eeb5696bb2bd15b5f009438
meltphace7 <brd9326@protonmail.com>
2023-11-30 19:47:42 -0800

    fixed styling issues in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import classes from "./Footer.module.css";
 import Image from "next/image";
 import logo from "../../public/assets/imgs/BDLOGO-white.png";
 
 const Footer: React.FC = () => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
   return (
     <footer className={classes.footer}>
       <div className={classes["logo-container"]}>
         <Image src={logo} alt="Brock Dallman logo" />
-        <p>Brock Dallman 2023</p>
+        <p>{`Brock Dallman ${currentYear}`}</p>
       </div>
     </footer>
   );
